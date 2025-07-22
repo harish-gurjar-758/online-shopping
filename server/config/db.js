421df81abc0fcf91f2b31ed875f148aa4a77efec
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-07-22 21:07:57 +0530

    create the connectDb {db} file for connect the mongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,21 @@
 //  # MongoDB connection
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useunidiedTopology: true
+        });
+
+        console.log("MongoDB Connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed : ", error.message);
+        process.exit(1);
+    }
+};
+
+export default db;
